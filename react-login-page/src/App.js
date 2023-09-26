7caa8385d1b7e387a7dfc5ce88040e43ba4ae535
saurabh kumar singh01~ <saurabh.e15299@cumail.in>
2023-09-26 14:13:34 +0530

    added react-login form

---
+++ react-login-page/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AgentLoginForm from './component/AgentLoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AgentLoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-login-page/src/component/AgentLoginForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./AgentLoginForm.css";
+// import AgentLogo from "../../asset/Black-Red.jpg";
+
+const AgentLoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [ userNamevalidataion, setUserNameValidation ] = useState(false);
+  const [ userPasswordValidation, setPasswordValidation ] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-form-container">
+      {" "}
+      <h2 className="agent-header">Agent Login</h2>{" "}
+      <p className="agent-para">
+        Hey,Enter your details to get sign in to your account
+      </p>{" "}
+      <form onSubmit={handleLogin}>
+        {" "}
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>{" "}
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            onMouseDown={(e) => setUserNameValidation(true)}
+          />{" "}
+          { username.length === 0 && userNamevalidataion ? <p style={{ color:"red", padding:"0px"}}>User Name is Required </p> : ""}
+        </div>{" "}
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>{" "}
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            onMouseDown={(e) => setPasswordValidation(true)}
+          />{" "}
+          { password.length === 0 && userPasswordValidation ? <p style={{ color:"red", padding:"0px"}}>User Password is Required </p> : ""}
+        </div>
+        <button type="submit">Login</button>{" "}
+      </form>{" "}
+    </div>
+  );
+};
+
+export default AgentLoginForm;
